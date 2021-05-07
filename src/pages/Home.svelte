cd149ca5c5d06bb57f4c9da01acde8b310a2e779
Tobias Faust <tf@ambimax.de>
2021-05-07 11:09:46 +0200

    Update icon

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -113,3 +113,17 @@
         </div>
     </ReorderList>
 </div>
+
+<div
+    style="width:512px;height:512px"
+    class="m-4 bg-gradient-to-bl from-yellow-300 to-yellow-400 text-white flex items-center justify-center"
+>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-96 w-96" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+        <path
+            fill-rule="evenodd"
+            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+            clip-rule="evenodd"
+        />
+    </svg>
+</div>
