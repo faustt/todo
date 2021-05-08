5a823f246e873b6a7fe3dc19a41577d59d010369
Tobias Faust <tf@ambimax.de>
2021-05-08 13:50:32 +0200

    Add service worker

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import "./global.postcss";
 import App from "./App.svelte";
+// @ts-ignore
+import { add } from "./components/Toasts.svelte";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm("There is a new version available. Want to update?")) {
+            updateSW(true);
+        }
+    },
+    onOfflineReady() {
+        add({
+            intent: "info",
+            text: "You can now work offline!",
+        });
+    },
+});
 
 const app = new App({
     target: document.getElementById("app"),
