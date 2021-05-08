1db015ee583b84abb28c1c5d9eea8c8dd8ee8b79
Tobias Faust <tf@ambimax.de>
2021-05-08 14:34:07 +0200

    Improve checked icon

--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -141,15 +141,21 @@
                         />
                     </svg>
                 </button>
-            {:else}
-                <input
-                    type="checkbox"
-                    class="mr-4"
-                    checked={item.isDone}
-                    on:click|preventDefault
-                    on:touchstart|stopPropagation
-                    on:mousedown|stopPropagation
-                />
+            {:else if item.isDone}
+                <div class="flex items-center justify-center p-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5 text-green-500"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </div>
             {/if}
         </div>
     </ReorderList>
