fa3c67d743461fee2d5050f7216ec163b2432ed2
FaustTobias <tobias@faustnet.de>
2021-10-05 11:57:14 +0200

    chore: Added `APP_BASE_PATH`environment variable to build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: process.env["APP_BASE_PATH"] ?? "/",
 	plugins: [
 		svelte(),
 		VitePWA({
